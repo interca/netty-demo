5e757da504194b1576544556b6a2e85ef32e23a9
hongsheng <1634340724@qq.com>
2023-07-18 21:02:19 +0800

    处理消息边界

---
+++ src/test/java/cn/itcast/netty/c5/Client.java
@@ -0,0 +1,26 @@
+package cn.itcast.netty.c5;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+
+/**
+ * 处理消息边界
+ */
+public class Client {
+    public static void main(String[] args) {
+        try {
+            SocketChannel sc = SocketChannel.open();
+            sc.connect(new InetSocketAddress("localhost", 8080));
+            SocketAddress address = sc.getLocalAddress();
+            //sc.write(Charset.defaultCharset().encode("hello\nworld\n"));
+            //sc.write(Charset.defaultCharset().encode("0123\n456789abcdef"));
+            sc.write(Charset.defaultCharset().encode("0123456789abcdef3333\n"));
+            System.in.read();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
